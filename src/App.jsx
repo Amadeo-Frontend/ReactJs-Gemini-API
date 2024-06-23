d7b8033aee654633a8959cb0d2304a2f90b80d2c
Amadeo Bon <amadeosph@outlook.com>
2024-06-23 18:06:14 -0300

    fix mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import{ useState } from "react";
+import { useState } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import BeatLoader from "react-spinners/BeatLoader";
 
@@ -39,10 +39,12 @@ function App() {
       const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
 
       const fileInputEl = document.querySelector("input[type=file]");
-      const imagePartsPromises = [...fileInputEl.files].map(fileToGenerativePart);
+      const imagePartsPromises = [...fileInputEl.files].map(
+        fileToGenerativePart
+      );
       const imageParts = await Promise.all(imagePartsPromises);
-      
-      const result = await model.generateContent([inputText,...imageParts]);
+
+      const result = await model.generateContent([inputText, ...imageParts]);
       const text = result.response.text();
 
       setLoading(false);
@@ -70,39 +72,55 @@ function App() {
   }
 
   return (
-    <main className="p-12 w-screen">
+    <main className="w-screen p-12 mx-auto">
       <div className="flex flex-col items-center justify-center">
-        <h1 className="py-10 text-4xl">Google AI Gemini Integration</h1>
-        <div className="card">
-        <input
-    className=" rounded-md border-none border-input bg-[#303133] text-gray-400 file:cursor-pointer file:py-3 file:border-0 file:bg-[#000] file:text-white file:text-sm file:font-medium"
-    type="file"
-    id="picture"
-  />
+        <h1 className="py-10 text-4xl">Ask Gemini AI</h1>
+        <div className="flex flex-col items-center p-2 card">
+          <div className="flex flex-col w-full mx-auto md:flex-row md:items-center">
+            <input
+            className=" rounded-md mb-2 border-none border-input bg-[#303133] text-gray-400 md:mb-0 file:cursor-pointer file:py-3 file:border-0 file:bg-[#000] file:text-white file:text-sm file:font-medium"
+            type="file"
+            id="picture"
+            lang="english"
+          />
           <input
             type="text"
             style={{ width: 400 }}
             value={inputText}
+            placeholder="Ask anything. What are you stuck on?"
             onChange={(e) => setInputText(e.target.value)}
             className="bg-[#303133] mb-2 px-4 py-3 outline-none w-[280px] text-white rounded-lg border-2 transition-colors duration-100 border-solid focus:border-[#596A95] border-[#2B3040] md:mb-0 md:mx-2"
           />
-          <span className="hidden md:inline">
-            {'|'}
-          </span>
+          <span className="hidden md:inline">{"|"}</span>
+          <div className="flex items-center justify-center whitespace-nowrap">
+            <button
+              className="bg-[#292929] border-2 border-[#3e3e3e] rounded-lg text-white px-5 py-3 text-sm md:text-base hover:border-[#fff] cursor-pointer transition mx-2"
+              disabled={loading}
+              onClick={() => fetchDataFromGeminiProAPI()}
+            >
+              {loading ? <BeatLoader size={8} color="#fff" /> : "Get PRO data"}
+            </button>
+            <button
+              className="bg-[#292929] border-2 border-[#3e3e3e] rounded-lg text-white px-5 py-3 text-sm md:text-base hover:border-[#fff] cursor-pointer transition"
+              disabled={loading}
+              onClick={() => fetchDataFromGeminiProVisionAPI()}
+            >
+              {loading ? (
+                <BeatLoader size={8} color="#fff" />
+              ) : (
+                "Get PRO Vision data"
+              )}
+            </button>
+          </div>
+          </div>
           
-          <button
-          className="bg-[#292929] border-2 border-[#3e3e3e] rounded-lg text-white px-6 py-3 text-base hover:border-[#fff] cursor-pointer transition mx-2"
-          disabled={loading} onClick={() => fetchDataFromGeminiProAPI()}>
-            {loading? <BeatLoader size={8} color="#fff" /> : "Get PRO data"}
-          </button>
-          <button
-          className="bg-[#292929] border-2 border-[#3e3e3e] rounded-lg text-white px-6 py-3 text-base hover:border-[#fff] cursor-pointer transition"
-            disabled={loading}
-            onClick={() => fetchDataFromGeminiProVisionAPI()}
-          >
-            {loading? <BeatLoader size={8} color="#fff" /> : "Get PRO Vision data"}
-          </button>
-          <div>{data && <div className="border border-gray-500 p-2 rounded-md my-2 max-w-screen-xl">Resposta: {data}</div>}</div>
+          <div className="flex flex-col p-2">
+            {data && (
+              <div className="max-w-screen-xl p-2 my-2 overflow-x-hidden border border-gray-500 rounded-md">
+                Resposta: {data}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </main>
